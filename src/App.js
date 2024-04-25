73fb10653840ecc9a4607b87696b319e416c437d
Mr Khaira <singhkhairaharkaran@gmail.com>
2024-04-25 04:06:39 -0400

    All invalid input setup done.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
-import { DevTool } from "@hookform/devtools";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ArrowIcon from "./icons/arrow-icon";
 
 function App() {
-  const form = useForm({
-    defaultValues: {
-      day: 1,
-      month: 1,
-      year: new Date().getFullYear(),
-    },
-  });
-  const { register, control, handleSubmit, watch } = form;
-  // const { errors } = formState;
+  const [dayErrorState, setDayError] = useState(false);
+  const [monthErrorState, setMonthError] = useState(false);
+  const [yearErrorState, setYearError] = useState(false);
+
+  const form = useForm(); // Remember we can have default values here.
+  const { register, handleSubmit, watch, formState } = form;
+  const { touchedFields } = formState;
   const watchDay = watch("day");
   const watchMonth = watch("month");
   const watchYear = watch("year");
 
-  const dateToday = new Date().getDate();
-  const monthToday = new Date().getMonth() + 1;
+  const dateToday = new Date().getDate(); // For calculaation of age
+  const monthToday = new Date().getMonth() + 1; // For calculaation of age
   const yearToday = new Date().getFullYear();
-  console.log("date - ", dateToday);
-  console.log("month - ", monthToday);
-  console.log("year - ", yearToday);
 
   function onSub(obj) {
     // Call the age calculation function.
-    console.log("dodod");
     console.log(obj);
   }
 
+  // This use effect is further using the watch method of the
+  // form object to
   useEffect(() => {
     const subscription = watch((value) => {
-      console.log(value);
+      if ([1, 3, 5, 7, 8, 10, 12].includes(value.month)) {
+        if (value.day < 1 || value.day > 31) {
+          setDayError(true);
+        } else {
+          setDayError(false);
+        }
+      }
+      if ([4, 6, 9, 11].includes(value.month)) {
+        if (value.day < 1 || value.day > 30) {
+          setDayError(true);
+        } else {
+          setDayError(false);
+        }
+      }
+      if ([2].includes(value.month)) {
+        if (value.year % 4 === 0) {
+          if (value.day < 1 || value.day > 29) {
+            setDayError(true);
+          } else {
+            setDayError(false);
+          }
+        } else {
+          if (value.day < 1 || value.day > 28) {
+            setDayError(true);
+          } else {
+            setDayError(false);
+          }
+        }
+      }
+
+      if (value.month < 1 || value.month > 12) {
+        setMonthError(true);
+      } else {
+        setMonthError(false);
+      }
+
+      if (value.year < 1000 || value.year > yearToday) {
+        setYearError(true);
+      } else {
+        setYearError(false);
+      }
     });
+
     return subscription.unsubscribe;
   }, [watchDay, watchMonth, watchYear]);
 
@@ -43,11 +80,25 @@ function App() {
         <form onSubmit={handleSubmit(onSub)} noValidate>
           <div className="containor-upper">
             <div>
-              <label type="DMY">DAY</label>
+              <label
+                type="DMY"
+                className={
+                  (touchedFields.day && !watchDay) || dayErrorState
+                    ? "redText"
+                    : ""
+                }>
+                DAY
+              </label>
               <br />
               <input
+                className={
+                  (touchedFields.day && !watchDay) || dayErrorState
+                    ? "inputError"
+                    : "inputNormal"
+                }
                 type="number"
                 id="day"
+                placeholder="DD"
                 {...register("day", {
                   valueAsNumber: true,
                   min: {
@@ -59,13 +110,35 @@ function App() {
                   required: "Day is required",
                 })}
               />
+              {touchedFields.day && !watchDay && (
+                <p className="errorText redText">This field is required</p>
+              )}
+              {dayErrorState && (
+                <p className={dayErrorState ? "errorText redText" : ""}>
+                  Must be a valid day
+                </p>
+              )}
             </div>
             <div>
-              <label type="DMY">MONTH</label>
+              <label
+                type="DMY"
+                className={
+                  (touchedFields.month && !watchMonth) || monthErrorState
+                    ? "redText"
+                    : ""
+                }>
+                MONTH
+              </label>
               <br />
               <input
+                className={
+                  (touchedFields.month && !watchMonth) || monthErrorState
+                    ? "inputError"
+                    : "inputNormal"
+                }
                 type="number"
                 id="month"
+                placeholder="MM"
                 {...register("month", {
                   valueAsNumber: true,
                   required: true,
@@ -77,13 +150,35 @@ function App() {
                   },
                 })}
               />
+              {touchedFields.month && !watchMonth && (
+                <p className="errorText redText">This field is required</p>
+              )}
+              {monthErrorState && (
+                <p className={monthErrorState ? "errorText redText" : ""}>
+                  Must be a valid month
+                </p>
+              )}
             </div>
             <div>
-              <label type="DMY">YEAR</label>
+              <label
+                type="DMY"
+                className={
+                  (touchedFields.year && !watchYear) || yearErrorState
+                    ? "redText"
+                    : ""
+                }>
+                YEAR
+              </label>
               <br />
               <input
+                className={
+                  (touchedFields.year && !watchYear) || yearErrorState
+                    ? "inputError"
+                    : "inputNormal"
+                }
                 type="number"
                 id="year"
+                placeholder="YYYY"
                 {...register("year", {
                   valueAsNumber: true,
                   required: true,
@@ -95,26 +190,34 @@ function App() {
                   },
                 })}
               />
+              {touchedFields.year && !watchYear && (
+                <p className="errorText redText">This field is required</p>
+              )}
+              {yearErrorState && (
+                <p className={yearErrorState ? "errorText redText" : ""}>
+                  Must be a valid year
+                </p>
+              )}
             </div>
           </div>
           <div className="line-and-arrow">
             {/*The line and the arrow goes here */}
             <p className="line"></p>
-            <button className="arrow">{">"}</button>
+            <button className="arrow">{<ArrowIcon></ArrowIcon>}</button>
           </div>
         </form>
         <div className="containor-lower">
           <div>
-            <span>--{/*The day useState val */}</span>
-            <span>{/*Day label */} day</span>
+            <span>--{/*The year useState val */}</span>
+            <span>{/*Year label */}year</span>
           </div>
           <div>
             <span>--{/*The month useState val */}</span>
             <span>{/*Month label */}month</span>
           </div>
           <div>
-            <span>--{/*The year useState val */}</span>
-            <span>{/*Year label */}year</span>
+            <span>--{/*The day useState val */}</span>
+            <span>{/*Day label */} day</span>
           </div>
         </div>
       </div>
@@ -122,97 +225,3 @@ function App() {
   );
 }
 export default App;
-
-//   return (
-//     <div className="App">
-//       <form onSubmit={handleSubmit(onSub)} noValidate>
-//         <div className="DMY-flex-upper-containor">
-//           <h1>Day - {watchDay}</h1>
-//           <div>
-//             <label type="DMY">DAY</label>
-//             <br />
-//             <input
-//               type="number"
-//               id="day"
-//               {...register("day", {
-//                 valueAsNumber: true,
-//                 min: {
-//                   value: 1,
-//                   // message: "Must be a valid date",
-//                 },
-//                 max: {
-//                   value: 31,
-//                   // message: "Must be a valid date",
-//                 },
-//                 required: "Day is required",
-//                 // message: "Must be between 1 to 31",
-//               })}
-//             />
-//             <p>{errors.day?.message}</p>
-//           </div>
-//           <div>
-//             <label type="DMY">MONTH</label>
-//             <br />
-//             <input
-//               type="number"
-//               id="month"
-//               {...register("month", {
-//                 valueAsNumber: true,
-//                 required: true,
-//                 min: {
-//                   value: 1,
-//                   // message: "Must be a valid month",
-//                 },
-//                 max: {
-//                   value: 12,
-//                   // message: "Must be a valid month",
-//                   // Add the watch.
-//                 },
-//                 // message: "Should be between 1 to 12",
-//               })}
-//             />
-//             <p>{errors.month?.message}</p>
-//           </div>
-//           <div>
-//             <label type="DMY">YEAR</label>
-//             <br />
-//             <input
-//               type="text"
-//               id="year"
-//               {...register("year", {
-//                 required: true,
-//                 pattern: {
-//                   value: /[12][0-9][0-9][0-9]/,
-//                   // This is yet a text solve this matter
-//                   // message: "Not a valid year",
-//                 },
-//               })}
-//             />
-//             <p>{errors.year?.message}</p>
-//           </div>
-//         </div>
-//         <div>
-//           {/*The line and the arrow goes here */}
-//           <hr />
-//           <button>arrow</button>
-//         </div>
-//       </form>
-//       <DevTool control={control} />
-//       <div className="DMY-display-lower-containor">
-//         <div>
-//           <span>--{/*The day useState val */}</span>
-//           <span>{/*Day label */} day</span>
-//         </div>
-//         <div>
-//           <span>--{/*The month useState val */}</span>
-//           <span>{/*Month label */}month</span>
-//         </div>
-//         <div>
-//           <span>--{/*The year useState val */}</span>
-//           <span>{/*Year label */}year</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default App;
